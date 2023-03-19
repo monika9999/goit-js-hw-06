1d0bd8646c63c520efc5db4058790042e4de4f5a
monika9999 <sowka16@gmail.com>
2023-03-19 20:21:45 +0100

    2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector("#ingredients");
+
+for (let item of ingredients) {
+  const element = document.createElement("li");
+  element.textContent = item;
+  element.classList.add("item");
+  list.append(element);
+};
+  
