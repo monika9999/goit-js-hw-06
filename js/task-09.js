2ee887501160db416dae69ad556c80f3ee9fbccd
monika9999 <sowka16@gmail.com>
2023-03-24 19:15:50 +0100

    09

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector("body");
+const color = document.querySelector(".color");
+const changeColor = document.querySelector(".change-color");
+
+changeColor.addEventListener("click", (event) => {
+  color.textContent = getRandomHexColor();
+  body.style.backgroundColor = color.textContent;
+});
