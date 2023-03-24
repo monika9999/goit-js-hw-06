39224328b76df0ef58981b6948d8866cceb18a7d
monika9999 <sowka16@gmail.com>
2023-03-24 19:11:29 +0100

    08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const loginForm = document.querySelector(".login-form");
 
+const inSubmit = (event) => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+};
+
+loginForm.addEventListener("submit", inSubmit);
